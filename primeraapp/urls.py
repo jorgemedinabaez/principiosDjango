21d1571418f518a75b608fc857fdc8f493535e60
jorgemedinabaez <jorgemedinabaez88@gmail.com>
2023-07-10 16:42:29 -0400

    primer commit, creación de primeraapp

---
+++ primeraapp/urls.py
@@ -0,0 +1,13 @@
+
+# from django.contrib import admin
+
+from django.urls import path
+
+from .views import IndexPageReview,obtener_fecha,menu_view
+
+urlpatterns = [
+    path('',IndexPageReview.as_view(),name='index'),
+    path('fecha/<str:name>/',obtener_fecha,name='fecha'),
+    path('menu/', menu_view,name='menu'),
+
+]
---
+++ primeraapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+import datetime
+
+class IndexPageReview(TemplateView):
+    template_name = 'index.html'
+
+def index(request):
+    return HttpResponse("Bienvenido a mi página, Hola Mundo.")
+
+def obtener_fecha(request,name):
+    fecha_actual = datetime.datetime.now()
+    context = {'fecha':fecha_actual,'name':name}
+    return render(request,'fecha.html',context)
+
+def menu_view(request):
+    template_name = 'menu.html'
+    return render(request,template_name)
